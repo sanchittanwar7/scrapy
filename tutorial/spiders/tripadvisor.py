dc3551d47bd1e2fe6f510d30fb0fb25896c2b7f0
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-07-03 15:59:13 +0530

    location argument added

--- tutorial/spiders/tripadvisor.py
+++ tutorial/spiders/tripadvisor.py
@@ -2,20 +2,25 @@
 import scrapy
 import random 
 from scrapy.http.request import Request
+import requests
 
 class TweetScraper(scrapy.Spider):
 	name = 'tripadvisor'
 	allowed_domains = ['tripadvisor.in']
 
-	def __init__(self):
-
+	def __init__(self, location):
+		self.location = location
 		self.query = ''
 		self.url = "https://www.tripadvisor.in/Hotels-g304552-%sShimla_Shimla_District_Himachal_Pradesh-Hotels.html"
+		self.start_url = "https://www.tripadvisor.com/TypeAheadJson?action=API&types=geonbrhdhoteltheme_park&filter=&legacy_format=true&urlList=true&strictParent=true&query=%s&max=6&name_depth=3&interleaved=true&scoreThreshold=0.5&strictAnd=false&typeahead1_5=true&disableMaxGroupSize=true&geoBoostFix=true&geoPages=&injectList=&neighborhood_geos=true&details=true&rescue=true&uiOrigin=trip_search_Hotels&source=trip_search_Hotels"
 		self.sno = 0
 		self.custom_url = ''
 
 	def start_requests(self):
-		url = self.url % ('')	
+		url = self.start_url % self.location
+		r = requests.get(url=url)
+		r = r.json()
+		url = 'https://www.tripadvisor.in/Hotels-g%s-Shimla_Shimla_District_Himachal_Pradesh-Hotels.html' % r[0]['value']
 		yield scrapy.Request(url, callback=self.parse_page)
 
 	def parse_page(self, response):
@@ -25,10 +30,10 @@ class TweetScraper(scrapy.Spider):
 			self.custom_url = url.split('Reviews')[0] + 'Reviews%s' + url.split('Reviews')[1]
 			yield scrapy.Request(url=url, callback=self.parse_reviews)
 
-		# self.sno += 30
-		# self.query = 'oa%s-' % self.sno
-		# url = self.url % (self.query)
-		# yield scrapy.Request(url, callback=self.parse_page)
+		self.sno += 30
+		self.query = 'oa%s-' % self.sno
+		url = self.url % (self.query)
+		yield scrapy.Request(url, callback=self.parse_page)
 
 	def parse_reviews(self, response):
 		reviews = response.css('div.review-container')
